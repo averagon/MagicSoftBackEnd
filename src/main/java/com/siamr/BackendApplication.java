6f15d6a691489623dd62726a8508a2a611bf3502
angelavg <angelaverag@gmail.com>
2023-12-06 13:02:03 -0600

    se agrego el cifrado de contraseñas

--- src/main/java/com/siamr/BackendApplication.java
+++ src/main/java/com/siamr/BackendApplication.java
@@ -2,12 +2,30 @@ package com.siamr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.siamr.config.JwtFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
+	}// MAIN
 
-}
+	
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter(){
+		FilterRegistrationBean<JwtFilter> registrationBean = 
+							new FilterRegistrationBean<>();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/api/servicios/*");
+		registrationBean.addUrlPatterns("/api/usuarios/*");
+		return registrationBean;
+	}//jwtFilter
+
+} // class BackendApplication
+
+
